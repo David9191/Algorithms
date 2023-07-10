030487a4df7bd4d2a189ef3bc8d95b41eb395455
YIM JI SEONG <72663806+David9191@users.noreply.github.com>
2023-07-11 01:00:15 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,11 +1,3 @@
 function solution(s) {
-    const str = s.split('');
-    const upper = [];
-    const lower = [];
-
-    for (let i = 0; i < str.length; i++)
-        if (str[i] >= 'A' && 'Z' >= str[i]) upper.push(str[i]);
-        else lower.push(str[i]);
-    return lower.sort((a,b)=>b.localeCompare(a)).join('') +
-        upper.sort((a,b)=>b.localeCompare(a)).join('');
+    return s.split('').sort().reverse().join('');
 }
